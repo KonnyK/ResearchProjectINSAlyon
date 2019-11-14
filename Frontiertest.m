f0030bf02f7212b79f96a394701c42042d738a51
Konny <konrad.kroeber@freenet.de>
2019-11-14 18:53:30 +0100

    update  to Frontiertest

--- Frontiertest.m
+++ Frontiertest.m
@@ -1,25 +1,37 @@
 world = zeros(20,20,20); % 0=libre, 1=occupée -1=inconnu
 
-cube1 = cube([1,1,1], [5,5,5]);
-world = insert(world, cube1, 1);
+cube1 = cube([5,5,1], [15,15,11]);
+cube2 = cube([3,3,1], [5,5,12]);
+cube3 = cube([7,7,11], [12,12,16]);
+cube4 = cube([20,20,20], [18,18,18]);
+world = insert(world, [cube1;cube2;cube3;cube4], 1);
 
-drone_pos = [3,3,10];
+drone_pos = [5,5,20];
 drone_maxDist = 10;
 siz = size(world);
 seenWorld = -ones(siz);
 
+X = drone_pos(1);
+Y = drone_pos(2);
+Z = drone_pos(3);
 for x = max(drone_pos(1)-drone_maxDist, 1):min(drone_pos(1)+drone_maxDist, siz(1))
     for y = max(drone_pos(2)-drone_maxDist, 1):min(drone_pos(2)+drone_maxDist, siz(2))
-        for z = 1:20
+        for z = max(drone_pos(3)-drone_maxDist, 1):drone_pos(3)
             if sqrt((x-drone_pos(1))^2 + (y-drone_pos(2))^2 + (z-drone_pos(3))^2) <= drone_maxDist
                 seenWorld(x,y,z) = world(x,y,z); 
                 if seenWorld(x,y,z) == 1
-                    plot3(x,y,z);
+                    X = [X;x];
+                    Y = [Y;y];
+                    Z = [Z;z];
                 end
             end
         end
     end
 end
+scale = 500 * ones(length(X),1);
+scale(1) = 100;
+scatter3(X,Y,Z,scale,'filled','s');
+axis([1,siz(1),1,siz(2),1,siz(3)]);
 
 
 function c = cube(P1, P2)
