4a44b7845a002baccb56e3f6d66335ae03d20b64
Konny <konrad.kroeber@freenet.de>
2019-11-15 16:43:54 +0100

    added raycasts to frontiertest

--- Frontiertest.m
+++ Frontiertest.m
@@ -1,38 +1,54 @@
-world = zeros(20,20,20); % 0=libre, 1=occupée -1=inconnu
+world = zeros(200,200,200); % 0=libre, 1=occupée -1=inconnu
 
-cube1 = cube([5,5,1], [15,15,11]);
-cube2 = cube([3,3,1], [5,5,12]);
-cube3 = cube([7,7,11], [12,12,16]);
-cube4 = cube([20,20,20], [18,18,18]);
+cube1 = cube([50,50,10], [150,150,110]);
+cube2 = cube([30,30,10], [50,50,120]);
+cube3 = cube([70,70,110], [120,120,160]);
+cube4 = cube([200,200,200], [180,180,180]);
 world = insert(world, [cube1;cube2;cube3;cube4], 1);
 
-drone_pos = [5,5,20];
-drone_maxDist = 10;
+drone_pos = [70,50,180];
+drone_maxDist = 500;
 siz = size(world);
 seenWorld = -ones(siz);
 
-X = drone_pos(1);
-Y = drone_pos(2);
-Z = drone_pos(3);
+X1 = drone_pos(1);
+Y1 = drone_pos(2);
+Z1 = drone_pos(3);
+X2 = zeros(0,1);
+Y2 = zeros(0,1);
+Z2 = zeros(0,1);
+
 for x = max(drone_pos(1)-drone_maxDist, 1):min(drone_pos(1)+drone_maxDist, siz(1))
     for y = max(drone_pos(2)-drone_maxDist, 1):min(drone_pos(2)+drone_maxDist, siz(2))
         for z = max(drone_pos(3)-drone_maxDist, 1):drone_pos(3)
-            if sqrt((x-drone_pos(1))^2 + (y-drone_pos(2))^2 + (z-drone_pos(3))^2) <= drone_maxDist
+            rc = raycast(world, [x,y,z], drone_pos, 3);
+            seeable = (rc(1) == drone_pos(1) && rc(2) == drone_pos(2) && rc(3) == drone_pos(3));
+            if distance([x,y,z], drone_pos) <= drone_maxDist && seeable
                 seenWorld(x,y,z) = world(x,y,z); 
                 if seenWorld(x,y,z) == 1
-                    X = [X;x];
-                    Y = [Y;y];
-                    Z = [Z;z];
+                    X1 = [X1;x];
+                    Y1 = [Y1;y];
+                    Z1 = [Z1;z];
                 end
+            end
+            if (world(x,y,z) == 1)
+                X2 = [X2;x];
+                Y2 = [Y2;y];
+                Z2 = [Z2;z];
             end
         end
     end
 end
-scale = 500 * ones(length(X),1);
+figure;
+scale = 20 * ones(length(X1),1);
 scale(1) = 100;
-scatter3(X,Y,Z,scale,'filled','s');
+scatter3(X1,Y1,Z1,scale,'filled','o');
 axis([1,siz(1),1,siz(2),1,siz(3)]);
 
+figure;
+scale = 20 * ones(length(X2),1);
+scatter3(X2,Y2,Z2,scale,'filled','o');
+axis([1,siz(1),1,siz(2),1,siz(3)]);
 
 function c = cube(P1, P2)
     points = zeros(0,3);
@@ -51,4 +67,29 @@ function ins = insert(map, pointcloud, value)
         map(pointcloud(i, 1), pointcloud(i, 2), pointcloud(i, 3)) = value;
     end
     ins = map;
+end
+
+function ray = raycast(map, From, To, depth)
+    if (depth <= 0)
+        ray = From;
+        return;
+    end
+    Dir = (To-From)/max(abs(To-From));
+    if (min(From == To) == 1)
+        ray = From;
+        return;
+    end
+    Next = [From(1)+Dir(1), From(2)+Dir(2), From(3)+Dir(3)];
+    while not(min(Next == To) == 1 || distance(Next, From) >= distance(From, To) || map(round(Next(1)),round(Next(2)),round(Next(3))) == 1)
+        Next = [Next(1)+Dir(1), Next(2)+Dir(2), Next(3)+Dir(3)];
+    end
+    if (min(Next == To) == 1 || distance(Next, From) >= distance(From, To))
+        ray = To;
+    elseif (map(round(Next(1)),round(Next(2)),round(Next(3))) == 1)
+        ray = raycast(map, Next, To, depth-1);
+    end
+end
+
+function dist = distance(P1, P2)
+    dist = sqrt((P1(1)-P2(1))^2 + (P1(2)-P2(2))^2 + (P1(3)-P2(3))^2);
 end
