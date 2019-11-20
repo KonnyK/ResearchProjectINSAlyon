a63b0ceb1025c90f30b185932aa1cd4b568fd6e1
Konny <konrad.kroeber@freenet.de>
2019-11-20 19:02:54 +0100

    added sphere function

--- Frontiertest.m
+++ Frontiertest.m
@@ -1,12 +1,15 @@
-
-world = zeros(200,200,200); % 0=libre, 1=occupée -1=inconnu
+% 0=libre, 1=occupée -1=inconnu
+world = zeros(200,200,200);
 
 %creating environment
 cube1 = cube([50,50,80], [150,150,110]);
 cube2 = cube([30,30,10], [50,50,120]);
 cube3 = cube([70,70,110], [120,120,160]);
 cube4 = cube([200,200,200], [180,180,1]);
-world = insert(world, [cube1;cube2;cube3;cube4], 1);
+environment1 = [cube1;cube2;cube3;cube4];
+environment2 = sphere([100,100,100],25);
+
+world = insert(world, [environment2], 1);
 
 drone_pos = [60,40,170]; %positione du spectateur
 drone_maxDist = 500; %distance maximale à laquelle points sonts detectés
@@ -54,6 +57,7 @@ seen(s:length(seen),:) = [];
 unseen(u:length(unseen),:) = [];
 front(f:length(front),:) = [];
 
+%{
 %graphique pour touts les points occupés détectés et non détectés
 figure;
 draw = [seen;unseen];
@@ -66,6 +70,7 @@ color(1,:) = [1,0,0]; %couleur du point du drone
 color(2:length(seen),:) = repmat([0,0,1],length(seen)-1,1); %couleur des points détectés
 scatter3(draw(:,1),draw(:,2),draw(:,3),scale,color,'filled','o');
 axis([1,siz(1),1,siz(2),1,siz(3)]);
+%}
 
 %graphique montrant points occupés détectés et points frontièrs
 figure;
@@ -80,7 +85,7 @@ scatter3(draw(:,1),draw(:,2),draw(:,3),scale,color,'filled','o');
 axis([1,siz(1),1,siz(2),1,siz(3)]);
 
 function points = cube(P1, P2)
-%retourne un nuage de points formant un cube avec les coins à P1(1x3) et P2(1x3)
+%rend un nuage de points formant un cube avec les coins à P1(1x3) et P2(1x3)
 %très lent à partir de 10^5 points
     points = zeros(abs(P1(1,1)-P2(1,1))*abs(P1(1,2)-P2(1,2))*abs(P1(1,3)-P2(1,3)),3);
     j = 1;
@@ -95,9 +100,26 @@ function points = cube(P1, P2)
     points(j:length(points),:) = [];
 end
 
+function points = sphere(pos, radius)
+%rend un nuage de points formant une sphere avec les centre à pos(1x3)
+    points = zeros(round((4/3)*pi*radius^3),3);
+    j = 1;
+    for x = pos(1,1)-radius:pos(1,1)+radius
+        for y = pos(1,2)-radius:pos(1,2)+radius
+            for z = pos(1,3)-radius:pos(1,3)+radius
+                if distance(pos, [x,y,z]) <= radius
+                    points(j,:) = [x,y,z];
+                    j = j+1;
+                end
+            end    
+        end
+    end
+    points(j:length(points),:) = [];
+end
+
 function ins = insert(map, pointcloud, value)
 %change la valeur des élements dans map indiqués par les élements de
-%pointcloud(~x3) et retourne la map changé
+%pointcloud(~x3) et rend la map changé
     for i = 1:length(pointcloud)
         map(pointcloud(i, 1), pointcloud(i, 2), pointcloud(i, 3)) = value;
     end
@@ -109,7 +131,7 @@ function ray = raycast(map, From, Towards, maxDist)
 %Towards(1x3). Tous points libres sont mis dans ray(~x3)
 %le raycast se termine ou au bord de la map ou au premier point occupé
 %il s'arrete aussi après la distance maxDist du point From
-%retourne touts points passés jusqu'à l'arret du raycast
+%rend touts points passés jusqu'à l'arret du raycast
     Dir = (Towards-From)/max(abs(Towards-From));
     if (min(From == Towards) == 1)
         ray = From;
@@ -142,7 +164,7 @@ function dist = distance(P1, P2)
 end
 
 function demi = demisphere(radius, pos)
-%retourne touts points apartenant à une demi sphere dessous pos(1x3)
+%rend touts points sur la surface d'une demi sphere dessous pos(1x3)
     demi = zeros(round(4*pi*radius^2),3);
     i = 1;
     for x = pos(1)-radius:pos(1)+radius
@@ -159,15 +181,30 @@ function demi = demisphere(radius, pos)
 end
 
 function b = isFrontier(map, P)
-%retourne 1 si P(1x3) est consideré comme frontière dans map(~x3) et 0 si non
+%rend 1 si P(1x3) est consideré comme frontière dans map(~x3) et 0 si non
     nbrs = zeros(6,3); %neighbours
     siz = size(map);
+    n=1;
+    %{
+    for x = max(P(1)-1,1):min(P(1)+1,siz(1))
+        for y = max(P(2)-1,1):min(P(2)+1,siz(2))
+            for z = max(P(3)-1,1):min(P(3)+1,siz(3))
+                if P(1) ~= x || P(2) ~= y || P(3) ~= z
+                    nbrs(n,:) = [x,y,z];
+                    n = n+1;
+                end
+            end
+        end
+    end
+    nbrs(n:end,:) = [];
+    %}
     nbrs(1,:) = [max(P(1)-1,1),P(2),P(3)];
     nbrs(2,:) = [P(1),max(P(2)-1,1),P(3)];
     nbrs(3,:) = [P(1),P(2),max(P(3)-1,1)];
     nbrs(4,:) = [min(P(1)+1,siz(1)),P(2),P(3)];
     nbrs(5,:) = [P(1),min(P(2)+1,siz(2)),P(3)];
     nbrs(6,:) = [P(1),P(2),min(P(3)+1,siz(3))];
+    %}
     
     unknown = 0;
     free = 0;
@@ -183,4 +220,7 @@ function b = isFrontier(map, P)
         end
     end
     b = (map(P(1),P(2),P(3))==0 && unknown>0 && occupied>0);
+    %37b = map(P(1),P(2),P(3))==-1 && occupied>0 && free>0;
+    %48b = map(P(1),P(2),P(3))==0 && unknown^(1/3)>occupied^(1/2) && unknown>0 && occupied>0;
+    %59b = map(P(1),P(2),P(3))==0 && unknown>occupied && unknown>0 && occupied>0;
 end
