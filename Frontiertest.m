63190bc853093090849f1bf4ceecdf6325e13be6
Konny <konrad.kroeber@freenet.de>
2019-11-22 10:33:05 +0100

    added FlightPath

--- Frontiertest.m
+++ Frontiertest.m
@@ -1,49 +1,58 @@
+%drone_pos = [60,40,170]; %position du spectateur
+drone_maxDist = 100; %distance maximale à laquelle points sonts detectés
+resolution = 150; %affecte le nombre de raycast faits et donc directement la resolution des points vus
+%FlightPath = [[(10:6:160)',(70:-2:20)',(180:-4:80)'];[(160:-1:140)',(20:8:180)',(80:6:200)']];
+FlightPath = [60,40,170];
+FPlength = size(FlightPath);
+FPlength = FPlength(1);
 % 0=libre, 1=occupée -1=inconnu
 world = zeros(200,200,200);
+siz = size(world); %pour racourcir du code plus tard
+seenWorld = -ones(siz);
 
 %creating environment
 cube1 = cube([50,50,80], [150,150,110]);
 cube2 = cube([30,30,10], [50,50,120]);
 cube3 = cube([70,70,110], [120,120,160]);
 cube4 = cube([200,200,200], [180,180,1]);
+cube5 = cube([90,70,120], [110,120,140]);
 environment1 = [cube1;cube2;cube3;cube4];
 environment2 = sphere([100,100,100],25);
+world = insert(world, environment1, 1);
+world = insert(world, cube5, 0);
 
-world = insert(world, [environment2], 1);
+for posCounter = 1:FPlength
+    drone_pos = FlightPath(posCounter,:);
+    FOV = demisphere(resolution, drone_pos); %nuage de points pour les raycast, le radius n'a rien a voir avec drone_maxDist
 
-drone_pos = [60,40,170]; %positione du spectateur
-drone_maxDist = 500; %distance maximale à laquelle points sonts detectés
-siz = size(world); %pour racourcir du code plus tard
-seenWorld = -ones(siz); 
-FOV = demisphere(150, drone_pos); %nuage de points pour les raycast, le radius n'a rien a voir avec drone_maxDist
-
-seen = zeros(length(FOV)+1,3); %tous points occupés et detectés par le drone + position du drone
-seen(1,:) = drone_pos; 
-unseen = zeros(siz(1)*siz(2)*siz(3),3); %touts points occupés et non detectés
-front = zeros(round((2/3)*pi*drone_maxDist^3),3); %touts points considérés comme frontière
-
-
-for index = 1:length(FOV) %raycast de drone_pose à chaque point du demisphere FOV
-    rc = raycast(world, drone_pos, [FOV(index,1),FOV(index,2),FOV(index,3)],drone_maxDist);
-    for r = 1:length(rc) %touts points passés par le raycast ne sont plus inconnus et donc enregistré dans seenWorld
-        seenWorld(rc(r,1), rc(r,2), rc(r,3)) = world(rc(r,1), rc(r,2), rc(r,3));
+    for index = 1:length(FOV) %raycast de drone_pose à chaque point du demisphere FOV
+        rc = raycast(world, drone_pos, [FOV(index,1),FOV(index,2),FOV(index,3)],drone_maxDist);
+        rcSize = size(rc);
+        for r = 1:rcSize(1) %touts points passés par le raycast ne sont plus inconnus et donc enregistré dans seenWorld
+            seenWorld(rc(r,1), rc(r,2), rc(r,3)) = world(rc(r,1), rc(r,2), rc(r,3));
+        end
     end
 end
-
+seen = zeros(length(FOV)+1,3); %tous points occupés et detectés par le drone + position du drone
+seen(1:FPlength,:) = FlightPath; 
+unseen = zeros(siz(1)*siz(2)*siz(3),3); %touts points occupés et non detectés
+front = zeros(round((2/3)*pi*drone_maxDist^3),3); %touts points considérés comme frontière
 f=1; %compteur de vraie longeur de front
-s=2; %compteur de vraie longeur de seen
+s=FPlength+1; %compteur de vraie longeur de seen
 u=1; %compteur de vraie longeur de unseen
 
 for x = 1:siz(1)
     for y = 1:siz(2)
         for z = 1:siz(3)
-            if isFrontier(seenWorld, [x,y,z])
-                front(f,:) = [x,y,z];
-                f = f+1;
-            end
+            
             if seenWorld(x,y,z) == 1
                 seen(s,:) = [x,y,z];
                 s = s+1;
+            elseif seenWorld(x,y,z) == 0
+                if isFrontier(seenWorld, [x,y,z])
+                    front(f,:) = [x,y,z];
+                    f = f+1;
+                end
             elseif world(x,y,z) == 1
                 unseen(u,:) = [x,y,z];
                 u = u+1;
@@ -57,32 +66,42 @@ seen(s:length(seen),:) = [];
 unseen(u:length(unseen),:) = [];
 front(f:length(front),:) = [];
 
-%{
+%}
 %graphique pour touts les points occupés détectés et non détectés
-figure;
+figure('name','Path_test_world');
 draw = [seen;unseen];
 Length = length(draw);
 scale = 5 * ones(Length,1); %largeur des points détectés
-scale(1) = 100; %largeur du point du drone
+scale(1:FPlength) = 50; %largeur du point drone
 scale(length(seen)+1:Length) = 10; %largeur des points non détectés
 color = 0.5 * ones(Length, 3); %couleur des points non détectés
-color(1,:) = [1,0,0]; %couleur du point du drone
-color(2:length(seen),:) = repmat([0,0,1],length(seen)-1,1); %couleur des points détectés
+color(1:FPlength,:) = repmat([1,0,0],FPlength,1); %couleur du point drone
+color(1+FPlength:length(seen),:) = repmat([0,0,1],length(seen)-FPlength,1); %couleur des points détectés
 scatter3(draw(:,1),draw(:,2),draw(:,3),scale,color,'filled','o');
 axis([1,siz(1),1,siz(2),1,siz(3)]);
 %}
 
 %graphique montrant points occupés détectés et points frontièrs
-figure;
+figure('name','Path_test_view');
 draw = [seen;front];
 Length = length(draw);
 scale = 5 * ones(Length,1); %largeur des points détectés
-scale(1) = 100; %largeur du point drone
+scale(1:FPlength) = 50; %largeur du point drone
 color = repmat([1,0.5,0],Length,1); %couleur des points frontièrs
-color(1,:) = [1,0,0]; %couleur du point drone
-color(2:length(seen),:) = repmat([0,0,1],length(seen)-1,1); %couleur des points détectés
+color(1:FPlength,:) = repmat([1,0,0],FPlength,1); %couleur du point drone
+color(1+FPlength:length(seen),:) = repmat([0,0,1],length(seen)-FPlength,1); %couleur des points détectés
 scatter3(draw(:,1),draw(:,2),draw(:,3),scale,color,'filled','o');
 axis([1,siz(1),1,siz(2),1,siz(3)]);
+
+%}
+
+
+
+
+
+
+
+
 
 function points = cube(P1, P2)
 %rend un nuage de points formant un cube avec les coins à P1(1x3) et P2(1x3)
@@ -182,14 +201,15 @@ end
 
 function b = isFrontier(map, P)
 %rend 1 si P(1x3) est consideré comme frontière dans map(~x3) et 0 si non
-    nbrs = zeros(6,3); %neighbours
+    c = 3; %cube size
+    nbrs = zeros(c^3 -1,3); %neighbours
     siz = size(map);
     n=1;
-    %{
-    for x = max(P(1)-1,1):min(P(1)+1,siz(1))
-        for y = max(P(2)-1,1):min(P(2)+1,siz(2))
-            for z = max(P(3)-1,1):min(P(3)+1,siz(3))
-                if P(1) ~= x || P(2) ~= y || P(3) ~= z
+    %}
+    for x = max(P(1)-(c-1)/2,1):min(P(1)+(c-1)/2,siz(1))
+        for y = max(P(2)-(c-1)/2,1):min(P(2)+(c-1)/2,siz(2))
+            for z = max(P(3)-(c-1)/2,1):min(P(3)+(c-1)/2,siz(3))
+                if (P(1) ~= x || P(2) ~= y || P(3) ~= z) && distance([x,y,z],P)<sqrt(3)
                     nbrs(n,:) = [x,y,z];
                     n = n+1;
                 end
@@ -197,14 +217,6 @@ function b = isFrontier(map, P)
         end
     end
     nbrs(n:end,:) = [];
-    %}
-    nbrs(1,:) = [max(P(1)-1,1),P(2),P(3)];
-    nbrs(2,:) = [P(1),max(P(2)-1,1),P(3)];
-    nbrs(3,:) = [P(1),P(2),max(P(3)-1,1)];
-    nbrs(4,:) = [min(P(1)+1,siz(1)),P(2),P(3)];
-    nbrs(5,:) = [P(1),min(P(2)+1,siz(2)),P(3)];
-    nbrs(6,:) = [P(1),P(2),min(P(3)+1,siz(3))];
-    %}
     
     unknown = 0;
     free = 0;
@@ -220,7 +232,6 @@ function b = isFrontier(map, P)
         end
     end
     b = (map(P(1),P(2),P(3))==0 && unknown>0 && occupied>0);
-    %37b = map(P(1),P(2),P(3))==-1 && occupied>0 && free>0;
-    %48b = map(P(1),P(2),P(3))==0 && unknown^(1/3)>occupied^(1/2) && unknown>0 && occupied>0;
-    %59b = map(P(1),P(2),P(3))==0 && unknown>occupied && unknown>0 && occupied>0;
+    %4 b = map(P(1),P(2),P(3))==0 && unknown>occupied && unknown>0 && occupied>0;
+    % b = map(P(1),P(2),P(3))==0 && unknown^(1/3)>occupied^(1/2) && occupied>0 && free<unknown;
 end
